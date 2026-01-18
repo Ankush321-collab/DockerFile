c5a0de5b68edc49b75352863c2dc5f543687a6bb
Ankush Adhikari <ankushadhikari321@gmail.com>
2026-01-18 15:56:41 +0530

    Delete Desktop/data_dashboard directory

--- Desktop/data_dashboard/src/app/Header/Header.tsx
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-export default function Header() {
-  return (
-    <header>
-      <h1>Data header</h1>
-    </header>
-  );
-}
--- Desktop/data_dashboard/src/app/dashboard/profile/Profile.tsx
+++
@@ -1,9 +0,0 @@
-import react from 'react';
-
-export default function (){
-    return (
-        <div>
-            profile page
-        </div>
-    );
-}
